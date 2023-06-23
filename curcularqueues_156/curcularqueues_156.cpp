78f9f5a2922bd62a14f1d2af5b713dc4b4bdefb6
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-23 09:33:52 +0700

    jika rear penihkembali ke awal array

--- curcularqueues_156/curcularqueues_156.cpp
+++ curcularqueues_156/curcularqueues_156.cpp
@@ -27,4 +27,13 @@ public:
 		if (FRONT == -1) {
 			FRONT = 0;
 			REAR = 0;
+		}
+		else {
+			//jika REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
