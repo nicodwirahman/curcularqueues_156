ab035093bb14656843aec9eab62b6a59c583e3e2
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-23 09:36:56 +0700

    menegecek antrian penuh apa enggak

--- curcularqueues_156/curcularqueues_156.cpp
+++ curcularqueues_156/curcularqueues_156.cpp
@@ -37,3 +37,11 @@ public:
 		}
 		queue_array[REAR] = num;
 	}
+
+	void remove(){
+		// cek apakah antrian kosong
+		if (FRONT == -1) {
+			cout << "queue underflow\n";
+			return;
+		}
+		cout << "\nThe element deleted from the queue is:" << queue_array[FRONT] << "\n";
