fda4b2e01d9b67540ac0f7f9bf752fb877558f4b
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-23 11:01:19 +0700

    posisi pront

--- curcularqueues_156/curcularqueues_156.cpp
+++ curcularqueues_156/curcularqueues_156.cpp
@@ -69,3 +69,14 @@ public:
 				cout << "queue is Empty\n";
 					return;
 	}
+
+			cout << "\nElements is the queue are...\n";
+
+			//jika FRONT_position <= rear_position, literasi dari front hingga rear
+			if (FRONT_position <= REAR_position) {
+				while (FRONT_position <= REAR_position) {
+					cout << queue_array[FRONT_position] << " ";
+					FRONT_position++;
+				}
+				cout << endl;
+				}
