032fe7f1d704704ec413201c67248eacee883cc9
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-23 11:11:29 +0700

    iterasi array dari awal

--- curcularqueues_156/curcularqueues_156.cpp
+++ curcularqueues_156/curcularqueues_156.cpp
@@ -86,6 +86,14 @@ public:
 				cout << queue_array[FRONT_position] << " ";
 				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			// iterasi dari awal array hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << " ";
+				FRONT_position++;
+			}
 			cout << endl;
 		}
 	}
