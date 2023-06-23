5c86d78c3b8787843469dbaf191ab4548e659da5
nicodwirahman <nico.dwi.ft22@mail.umy.ac.id>
2023-06-23 09:13:37 +0700

    MEMBENARKAN TUTUP KURANG KURAWAL DI LINE 4

--- curcularqueues_156/curcularqueues_156.cpp
+++ curcularqueues_156/curcularqueues_156.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 using namespace std;
 
-class queues(
-	int front, rear, max = 5;
-int queue_array[5];
-	)
+class queues {
+	int FRONT, REAR, max = 5;
+	int queue_array[5];
+
+public:
+	queues() {
+		FRONT = -1;
+	}
+};
